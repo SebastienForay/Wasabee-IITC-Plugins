0483ba2ce8f95c3d66b2a231af503dff6fffe04a
Sebastien - fisher01 <contact@sebastienforay.fr>
2022-02-05 12:26:13 -0500

    Add Kureze Phase 2 plugin

---
+++ KurezeEffect-Phase2/wb-kureze-p2.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author         fisher01
+// @name           IITC plugin: Wasabee: Mark Portals with Kureze Effect ornaments
+// @category       Misc
+// @version        0.1.0
+// @description    Automark Portals for Kureze Effect Phase 2
+// @id             wb-kureze
+// @updateURL      https://github.com/SebastienForay/Wasabee-IITC-Plugins/raw/main/KurezeEffect-Phase2/wb-kureze-p2.meta.js
+// @downloadURL    https://github.com/SebastienForay/Wasabee-IITC-Plugins/raw/main/KurezeEffect-Phase2/wb-kureze-p2.user.js
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
